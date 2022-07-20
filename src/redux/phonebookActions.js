87783751e9b8d6404c3113ecf32e0a849486a4d9
Maxi <mmaximonchuk@gmail.com>
2022-07-21 00:10:49 +0300

    update: deploy

--- src/redux/phonebookActions.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const setContacts = createAction('phonebook/setContacts');
-export const addContact = createAction('phonebook/addContact');
-export const deleteContact = createAction('phonebook/deleteContact');
-export const setFilterTerm = createAction('phonebook/setFilterTerm');
